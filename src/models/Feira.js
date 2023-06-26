8ff985ca9eb7afa4c4a8c2dc91176300ae835625
Ana Beatriz <ana_frota@econeteditora.com.br>
2023-06-26 17:10:27 -0300

    fix feira Model

--- src/models/Feira.js
+++ src/models/Feira.js
@@ -5,12 +5,15 @@ const feiraSchema = new mongoose.Schema(
        id:{type: Object},
        nome: {type: String, required: true},
        bairro: {type: String, required: true},
-       tipo: {type: Array, required:true},
+       tipo: {type: String, required:true},
        endereco: {type: String, required: true},
-       referencia: {type: String, required: true},
-       dia_da_semana : {type: String, required:true},
-       horario_inicial: {type: Date, required:true},
-       horario_final: {type: Date, required:true},
+       referencia: {type: String, required: false},
+       observacao: {type: String, required: false},
+       telefone: {type: String, required: false},
+       link_maps: {type: String, required: true},
+       dia_da_semana : {type: Array, required:true},
+       horario_inicial: {type: String, required:true},
+       horario_final: {type: String, required:true},
 
 }
 );
